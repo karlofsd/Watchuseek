a98aaaea44bb8cb79ef1c43f521425810451079f
Karlo Soberon <sober.karlo@gmail.com>
2020-09-01 17:49:51 -0300

    finish

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -9,4 +9,12 @@ server.get('/', (req, res, next) => {
 		.catch(next);
 });
 
+server.get('/:category',(req,res) => {
+	Product.findAll({
+		where:{categoryId:Number(req.params.category)} //El atributo category(foreing Key) depende del asignado a los modelos de BD o si esta asociado(belongsto)
+	}).then(products => {
+		res.json(products)
+	})
+})
+
 module.exports = server;
