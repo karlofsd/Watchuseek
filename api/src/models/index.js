9e74cdabe01527d7e8e8ec59e7cc7cf71a7b7da3
AgustinCollante <agustine.collante@gmail.com>
2020-09-01 15:28:10 -0300

    Relaciones

--- api/src/models/index.js
+++
@@ -1,8 +0,0 @@
-import db from './_db';
-import Categories from './Categories';
-import Product from './Product';
-
-Categories.hasMany(Product);
-Product.belongsTo(Categories);
-
-export default db;
