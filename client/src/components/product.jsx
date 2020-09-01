25de704fde734e879dcbb9b92cf11a37e2cc6fa8
Luciano <lucianooo222@gmail.com>
2020-09-01 18:21:39 -0300

    Component product created

---
+++ client/src/components/product.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Product = ({ name, price, description }) => {
+  return (
+    <div className="card text-center shadow col-7 p-0 mx-auto">
+      <div className="card-header">
+        {name}
+      </div>
+      <div className="card-body">
+        <img className='card-img w-25' src='https://i.pinimg.com/originals/45/7b/e2/457be2ce48176066fa700e18ae544852.jpg' />
+        <hr />
+        <h5 className="card-title">Price: $ {price}</h5>
+        <p className="card-text">Description: {description}</p>
+        <a href="#" className="btn btn-primary rounded-pill">Buy Now</a>
+      </div>
+      <div className="card-footer text-muted">
+        2 days ago
+      </div>
+    </div>
+  );
+}
+
+export default Product;
