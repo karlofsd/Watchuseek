642fe5f9da75fc7ed9e01569f340a66cb5056580
AgustinCollante <agustine.collante@gmail.com>
2020-09-02 01:26:11 -0300

    Componente Crud

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from "./components/searchBar/searchBar";
 import Product from "./components/product.jsx";
 import Nav from "./components/Nav/Nav.js";
+import Crud from "./components/crud/crud.js";
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className="App">
         <div className='container mt-5'>
-         <Nav />
+         <Crud />
         </div>
     </div>
   );
--- client/src/components/crud/crud.js
+++ client/src/components/crud/crud.js
@@ -2,22 +2,60 @@ import React, {useState} from "react";
 import "./crud.css";
 
 const Crud = () => {
-    const [title, setTitle] = useState();
+    const [input, setInput] = useState({
+        title: "",
+        description: "",
+        price: "",
+        stock: "" ,
+        image: "",
+        category: "",
+    });
+
+    const handleInputChange = function(e) {
+        setInput({
+          ...input,
+          [e.target.name]: e.target.value
+        });
+      }
+
+      const handleSubmit = function (e){
+        e.preventDefault();
+        alert("Agregado exitosamente");
+      }
+
 
     return (
-    <form>
-        <div>
+    <form onSubmit = {(e) => handleSubmit(e)} >
+        <div className ="content">
+        <div >
             <label>Titulo:</label>
-            <input type = "text" name = "title"  />
+            <br/>
+            <input type = "text" name = "title" onChange={(e) =>handleInputChange(e)} value = {input["title"]} />
         </div>
         <div>
-            <label>Descripción:</label>
-            <input type = "text" name = "description"  />
+            <label>Descripción:</label><br/>
+            <input type = "text" name = "description" onChange={(e) =>handleInputChange(e)} value = {input["description"]} />
         </div>
         <div>
-            <label>Precio:</label>
-            <input type = "number" name = "description"  />
+            <label>Precio:</label><br/>
+            <input type = "number" name = "price" onChange={(e) =>handleInputChange(e)} value = {input["price"]}/>
+        </div>
+        <div>
+            <label>Stock:</label><br/>
+            <input type = "number" name = "stock" onChange={(e) =>handleInputChange(e)} value = {input["stock"]} />
+        </div>
+        <div>
+            <label>Imagen:</label><br/>
+            <input type = "text" name = "image" onChange={(e) =>handleInputChange(e)} value = {input["image"]} />
+        </div>
+        <div>
+            <label>Categoria:</label><br/>
+            <input type = "text" name = "category" onChange={(e) =>handleInputChange(e)} value = {input["category"]} />
+        </div>
+        <button type = "submit" >Boton</button>
         </div>
     </form>
-    )
-}
+    );
+};
+
+export default Crud;
