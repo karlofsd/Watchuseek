87f1f52706253a723cc0d4a33ec2245cda2c05fb
Luciano <lucianooo222@gmail.com>
2020-09-03 00:02:05 -0300

    Route delete categroy created

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -1,7 +1,7 @@
 const server = require('express').Router();
 const { Categories } = require('../db.js');
 
-server.get('/category/', async (request, response) => {
+server.post('/category/', async (request, response) => {
   const { name, description } = request.body;
   // Crear y guardar nueva categoria
   const newCategory = await Categories.findOrCreate({
@@ -13,6 +13,21 @@ server.get('/category/', async (request, response) => {
   // Objeto creado
   response.status(201).json({
     newCategory
+  });
+
+});
+
+server.delete('/category/:id', async (request, response) => {
+  const { id } = request.params;
+  // Eliminar categoria por ID
+  await Categories.destroy({
+    where: {
+      id
+    }
+  });
+  // Responder con mensaje personalizado
+  response.status(200).json({
+    message: 'Category deleted.'
   });
 
 });
