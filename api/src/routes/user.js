46a11765d377b7f1a9eb2ccad8fa5adf5e8a9cc9
Luciano <lucianooo222@gmail.com>
2020-09-10 00:32:32 -0300

    Se agrego ruta para listar todas las ordenes de un usuario

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,7 +1,7 @@
 const server = require('express').Router();
 const { Users } = require('../db.js');
-const {Carrito} = require('../db.js');
-const { Sequelize:{Op}} = require('sequelize');
+const { Carrito } = require('../db.js');
+const { Sequelize: { Op } } = require('sequelize');
 
 
 server.post("/", (req, res) => {
@@ -64,68 +64,92 @@ server.delete('/:id', async (request, response) => {
 
 
 
-server.post("/:UserId/carrito", (req, res)=>{
+server.post("/:UserId/carrito", (req, res) => {
   var id = req.params.UserId
-  const {name, price, quantity, status} = req.body;
+  const { name, price, quantity, status } = req.body;
 
-  Carrito.findOrCreate({where:{userId:id,name, price, quantity,status}})
-  .then(order=>{
-    res.status(201).json(order)
-  })
-  .catch(err=>{
-    res.status(404).json('No se pudo agregar')
-  })
+  Carrito.findOrCreate({ where: { userId: id, name, price, quantity, status } })
+    .then(order => {
+      res.status(201).json(order)
+    })
+    .catch(err => {
+      res.status(404).json('No se pudo agregar')
+    })
 })
 
 
 
-server.get("/:UserId/carrito", (req, res)=>{
+server.get("/:UserId/carrito", (req, res) => {
   var id = req.params.UserId
 
-  Carrito.findAll({where:{
-    userId:id,
-    status:{ [Op.or] : ["carrito" , "procesando" , "creada"]} 
-  }})
-  .then(orden=>{
-    res.status(201).send(orden)
+  Carrito.findAll({
+    where: {
+      userId: id,
+      status: { [Op.or]: ["carrito", "procesando", "creada"] }
+    }
   })
-  .catch(err=>{
-    res.status(404).send('No se encontraron pedidos o hubo un error!')
-  })
+    .then(orden => {
+      res.status(201).send(orden)
+    })
+    .catch(err => {
+      res.status(404).send('No se encontraron pedidos o hubo un error!')
+    })
 })
 
 
 
-server.delete('/:UserId/carrito', (req, res)=>{
+server.delete('/:UserId/carrito', (req, res) => {
   let id = req.params.UserId
 
-  Carrito.destroy({where:{
-    userId:id
-  }})
-  .then(orden=>{
-    res.status(201).send('Se ha vaciado el carrito')
+  Carrito.destroy({
+    where: {
+      userId: id
+    }
   })
-  .catch(err => {
-    res.status(404).send('Hubo un error')
-  })
+    .then(orden => {
+      res.status(201).send('Se ha vaciado el carrito')
+    })
+    .catch(err => {
+      res.status(404).send('Hubo un error')
+    })
 })
 
 
-server.put('/:UserId/carrito/:id', (req, res) =>{
+server.put('/:UserId/carrito/:id', (req, res) => {
   let userId = req.params.UserId
   let id = req.params.id
-  const {name, price, quantity, status} = req.body
+  const { quantity } = req.body
 
-  Carrito.update({name, price, quantity, status}, { where:{ userId, id}})
-  .then(orden => {
-    res.status(201).send(orden)
-  })
-  .catch (err => {
-    console.log(err)
-    res.status(404).send(err)
-  })
+  Carrito.update({ quantity }, { where: { userId, id } })
+    .then(orden => {
+      res.status(201).send(orden)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(404).send(err)
+    })
 })
 
+server.get('/:id/orders', async (request, response) => {
+  const { id } = request.params;
+
+  const orders = await Carrito.findAll({
+    where: {
+      userId: id
+    }
+  });
+
+  if (!orders) {
+    return response.status(400).json({
+      message: 'Orders not found.'
+    });
+  }
+
+  return response.status(200).json({
+    orders
+  });
+
+});
 
 
 
