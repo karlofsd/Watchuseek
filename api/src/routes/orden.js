59f75626b6ba9ba5abaf4a6e7e179a743285e76c
Luciano <lucianooo222@gmail.com>
2020-09-10 00:27:50 -0300

    Se agregaron tareas 46 47

--- api/src/routes/orden.js
+++ api/src/routes/orden.js
@@ -1,5 +1,55 @@
 const server = require('express').Router();
 const { Carrito } = require('../db.js');
 
+server.get('/', async (request, response) => {
+  const { status } = request.query;
+  let condition = {
+    where: {
+      status
+    }
+  };
+
+  const orders = await Carrito.findAll(status && condition);
+
+  if(!orders) {
+    return response.status(400).json({
+      message: 'Orders not found' 
+    });
+  }
+
+  return response.status(200).json({
+    orders
+  });
+
+});
+
+server.get('/:id', async (request, response) => {
+  const { id } = request.params;
+  const orderFound = await Carrito.findByPk(id);
+
+  if (!orderFound) {
+    return response.status(400).json({
+      message: `The order with id: ${id} doesn't exist.`
+    });
+  }
+
+  return response.status(200).json({
+    orderFound
+  });
+  
+});
+
+
+server.put('/:id', (req, res)=>{
+  var id = req.params.id
+  const {name, price, quantity, status} = req.body
+  Carrito.update({name, price, quantity, status}, {where: {id}})
+  .then(respons=>{
+    res.status(201).send(respons) 
+  })
+  .catch(err=>{
+    res.status(404).send('No se pudo completar con exito!')
+  }) 
+})
 
 module.exports = server;
